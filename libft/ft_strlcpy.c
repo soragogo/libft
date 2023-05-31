028f151f8bec2d33402eed283ca871b3a3ac3fac
Emu Kamada <ekamada@c1r22s9.42tokyo.jp>
2023-05-31 11:51:51 +0900

    the second tester failing

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ekamada <ekamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 13:40:54 by ekamada           #+#    #+#             */
-/*   Updated: 2023/05/29 14:36:57 by ekamada          ###   ########.fr       */
+/*   Updated: 2023/05/31 11:48:30 by ekamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,24 @@ size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {	
 	size_t count;
 
+	if(dstsize == 0)
+		return(ft_strlen(src));
 	count = ft_strlen(src);
-	while (dstsize > 1)
+	while (dstsize > 1 && *src)
 	{
 		*dst++ = *src++;
-		dstsize--;
+		dstsize --; 
 	}
-	if(dstsize == 1)
-		*dst = 0;
+	*dst = 0;
 	return (count);
+	
 }
+
+// int main()
+// {
+
+// 	char src[] = "coucou";
+// 	char dest[10]; memset(dest, 'A', 10);
+// 	ft_strlcpy(dest, src, 0);
+// 	printf("%s", dest);
+// }
