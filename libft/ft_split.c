57caab5d0992679c1374764bbef17563a76d1a35
Emu Kamada <ekamada@c1r22s8.42tokyo.jp>
2023-06-01 20:32:22 +0900

    split success:)

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,33 +6,111 @@
 /*   By: ekamada <ekamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 13:09:01 by ekamada           #+#    #+#             */
-/*   Updated: 2023/05/31 21:48:38 by ekamada          ###   ########.fr       */
+/*   Updated: 2023/06/01 20:26:30 by ekamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-<<<<<<< Updated upstream
+int ft_untilc_len(char const *s, char c)
+{
+	int i = 0;
+
+	while(s[i] != c && s[i] != 0)
+		i++;
+	return (i);
+}
+
+
+char const *ft_ctrim(char const *s, char c)
+{
+	if (*s == c)
+	{
+		while(*s == c)
+			s++;
+	}
+	return(s);
+}
+
+
+int ft_count_blocs(char const *s, char c)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if(*s != c)
+		{
+			count ++;
+			while (*s != c && *s)
+				s++;
+		}
+		while (*s == c)
+			s++;
+	}
+	return (count);
+}
 
 char **ft_split(char const *s, char c)
 {
+	int count = 0;
+	int i = 0;
+	char **matrix;
+
+	if (*s == 0)
+	{
+		matrix = (char**)malloc(sizeof(char *));
+		matrix[0] = NULL;
+		return (matrix);
+	}
+	if (c == 0)
+	{
+		matrix = malloc(sizeof(char *) * 2);
+		if(matrix == NULL)
+			return(NULL);
+		matrix[0] = malloc(sizeof(char)* (ft_strlen(s) + 1));
+		if(matrix[0] == NULL)
+		{
+			free(matrix);
+			return(NULL);
+		}
+		ft_strlcpy(matrix[0], s, ft_strlen(s) + 1);
+		matrix[1] = NULL;
+		return(matrix);
+	}
+	count = ft_count_blocs(s, c);
+	matrix = (char **)malloc(sizeof(char *) * (count + 1));
+	if(matrix == NULL)
+		return(NULL);
+	count = 0;
+	while(*s)
+	{
+		s += count;
+		s = ft_ctrim(s, c);
+		if(*s == 0)
+			break;
+		count = ft_untilc_len(s, c);
+		matrix[i] = malloc(sizeof(char) * (count + 1));
+		if (matrix[i] == NULL)
+		{
+			while(i > 0)
+				free(matrix[--i]);
+			free(matrix);
+			return (NULL);
+		}
+		ft_strlcpy(matrix[i], s, count + 1);
+		i++;
+	}
+	matrix[i] = NULL;
+	return (matrix);
 }
-=======
-// char **ft_split(char const *s, char c)
-// {
-// 	char *s_ = s;
-// 	while (*s)
-// 	{
-// 		while (*s == c)
-// 			s++;
-// 	}
-// }
->>>>>>> Stashed changes
 
 // int main()
 // {
-// 	char *s = "a1a22a333a4444a55555a666666a7777777aa";
-// 	char c = 'a';
+// 	char *s = "tripouille";
+// 	char c = 0;
 
 // 	char **matrix = ft_split(s, c);
 // 	for(int i = 0;matrix[i] != NULL; i++)
--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ekamada <ekamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:44:53 by ekamada           #+#    #+#             */
-/*   Updated: 2023/05/31 21:26:05 by ekamada          ###   ########.fr       */
+/*   Updated: 2023/06/01 20:32:01 by ekamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ char *ft_strdup(const char *str)
 	if (cpy == NULL)
 		return NULL;
 	ft_strlcpy(cpy, str, i + 1);
-	printf("%s", cpy);
 	return (cpy);
 }
 
