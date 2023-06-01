08f0ae7429cb6f88b5185a94819d09cd24f1d604
Emu Kamada <ekamada@c1r22s8.42tokyo.jp>
2023-06-01 22:24:28 +0900

    norm

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,56 +6,64 @@
 /*   By: ekamada <ekamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:44:32 by ekamada           #+#    #+#             */
-/*   Updated: 2023/06/01 22:03:16 by ekamada          ###   ########.fr       */
+/*   Updated: 2023/06/01 22:24:14 by ekamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int check_overflow(const char *str, int minus, size_t count)
+static int	check_overflow(const char *str, int minus, size_t count)
 {
-	char *str_ = (char *)str;
-	if(count > ft_strlen("9223372036854775807"))
-		return(-1);
-	else if(count == ft_strlen("9223372036854775807"))
+	char	*str_;
+
+	str_ = (char *)str;
+	if (count > ft_strlen("9223372036854775807"))
+		return (-1);
+	else if (count == ft_strlen("9223372036854775807"))
 	{
-		if(minus < 0)
+		if (minus < 0)
 		{
-			printf("%d",ft_strncmp(str_, "9223372036854775808", 19));
-			if((ft_strncmp(str_, "9223372036854775808", 19)) > 0)
-				return(0);
+			if ((ft_strncmp(str_, "9223372036854775808", 19)) > 0)
+				return (0);
 		}
-		else if((ft_strncmp(str_, "9223372036854775807", 19)) > 0)
-				return(-1);
+		else if ((ft_strncmp(str_, "9223372036854775807", 19)) > 0)
+			return (-1);
 	}
-	return(1);
+	return (1);
 }
 
-size_t num_len(const char *str)
+static size_t	num_len(const char *str)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
-	while(str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9')
 		i++;
-	return(i);
+	return (i);
+}
+
+static long long int	num_maker(const char *str)
+{
+	long long int	num;
+
+	num = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		num = num * 10 + *str - '0';
+		str++;
+	}
+	return (num);
 }
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	long long int j;
-	int	minus;
-	size_t count;
+	int				minus;
+	size_t			count;
 
-	i = 0;
-	j = 0;	
 	count = 0;
 	minus = 1;
-	while ((*str == ' ')
-			||(*str == '\t')||(*str == '\n')
-			||(*str == '\v')||(*str == '\f')
-			||(*str == '\r')||(*str == ' '))
+	while ((*str == '\t') || (*str == '\n') || (*str == '\v')
+		|| (*str == '\f') || (*str == '\r') || (*str == ' '))
 		str++;
 	if (*str == '-')
 	{
@@ -64,19 +72,13 @@ int	ft_atoi(const char *str)
 	}
 	else if (*str == '+')
 		str++;
-	while(*str == '0')
+	while (*str == '0')
 			str++;
 	count = num_len(str);
 	if (check_overflow(str, minus, count) != 1)
-		return(check_overflow(str, minus, count));
-	while (*str >= '0' && *str <= '9')
-	{
-		j = j *10 + *str - '0';
-		str++;
-	}
-	return ((int)j * minus);
+		return (check_overflow(str, minus, count));
+	return ((int)num_maker(str) * minus);
 }
-
 // int main()
 // {
 // 	printf("%d", ft_atoi("18446744073709551614"));
